1c5c5c0b175536851f1b435adc0bcedb3c124a8d
Venkatesan Srinivasan <vijayvenkatesans@vvs-pheonix.(none)>
2013-03-10 19:10:21 +0530

    Added fedora new installation helper script

---
+++ fednewinstall.sh
@@ -0,0 +1,13 @@
+yum install -y gstreamer-plugins-good gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-ffmpeg
+yum install -y pidgin wine vlc vim ctags
+yum install -y k3b
+yum install -y liveusb-creator unetbootin
+yum install -y mplayer mencoder
+yum install -y apache2 httpd mysql mysql-server mysql-admin php php5
+yum install -y gcc g++ gcc-c++ flex bison python perl
+yum install -y java-1.6.0-openjdk java-1.6.0-openjdk-javadoc icedtea-web
+yum install -y eclipse git svn eclipse-cdt
+yum install -y filezilla
+yum install -y libreoffice
+yum install -y chmsee cheese gimp audacity
+
